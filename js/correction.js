1962371beedf62f407c2d07572c0336a40a57f83
AgathePons <ponsagathe37@gmail.com>
2021-11-23 16:27:30 +0100

    events listener to handle number of dices and play button

--- js/correction.js
+++ js/correction.js
@@ -4,6 +4,8 @@ dealer.classList.add('board');
 dealer.id = 'dealer';
 app.insertAdjacentElement('beforeend', dealer);
 
+var chosenNbOfDice = 6;
+
 /**
  * Fonction qui génère un dé 6 avec un nombre aléatoire et l'ajoute à l'élément ayant l'id player
  * @params {string} Le côté où on lancera le dé
@@ -12,13 +14,18 @@ function addDice(side) {
   var dice = document.createElement('div');
   dice.classList.add('dice');
   var roll = generateRandomNumber();
-  console.log(roll);
+  //console.log(roll);
   var position = `-${(roll - 1) * 100}px`;
   dice.style.backgroundPositionX = position;
   var board = document.getElementById(side);
   board.appendChild(dice);
   // on aurait pu utiliser insertAdjacentHTML pour plus de choix
   //board.insertAdjacentElement('beforeend', dice);
+
+  dice.addEventListener('click', function(event) {
+    event.stopPropagation();
+    console.log('on a cliqué sur le dé');
+  });
 }
 
 /**
@@ -66,4 +73,46 @@ function resetBoard() {
   }
 }
 
-throwDice(3);
+var playButton = document.getElementById('play-button');
+
+function startGame() {
+  throwDice(chosenNbOfDice);
+}
+
+playButton.addEventListener('click', function(event) {
+  event.stopPropagation();
+  startGame(event);
+});
+
+// on selectionne le range input
+var rangeInput = document.getElementById('dice-range');
+rangeInput.value = `${chosenNbOfDice}`;
+
+// on écout les change sur l'input
+rangeInput.addEventListener('change', function(event) {
+  console.log(event.target.value);
+  chosenNbOfDice = parseInt(event.target.value);
+})
+
+document.body.addEventListener('keyup', function(event) {
+  console.log(event);
+  if (event.code.toLocaleLowerCase() === 'enter') {
+    startGame();
+  }
+});
+
+// attention au bubbling : un evennement qui se propage à ses parents, et donc qui peut déclencher d'autres événements
+document.getElementById('app').addEventListener('click', function(event) {
+  event.stopPropagation();
+  console.log("on a cliqué sur app");
+});
+
+document.body.addEventListener('click', function() {
+  console.log("on a cliqué sur le body");
+  //resetBoard();
+});
+
+
+
+
+
