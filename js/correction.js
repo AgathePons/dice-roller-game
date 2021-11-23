a101a6a0dd0ba1ad11a0827192d1dc03199fb6fc
AgathePons <ponsagathe37@gmail.com>
2021-11-23 13:04:44 +0100

    some little modif almost nothing

--- js/correction.js
+++ js/correction.js
@@ -1,16 +1,24 @@
+var app = document.getElementById('app');
+var dealer = document.createElement('div');
+dealer.classList.add('board');
+dealer.id = 'dealer';
+app.insertAdjacentElement('beforeend', dealer);
+
 /**
  * Fonction qui génère un dé 6 avec un nombre aléatoire et l'ajoute à l'élément ayant l'id player
+ * @params {string} Le côté où on lancera le dé
  */
-function addDice() {
+function addDice(side) {
   var dice = document.createElement('div');
   dice.classList.add('dice');
   var roll = generateRandomNumber();
   console.log(roll);
-  dice.style.backgroundPositionX = `-${(roll - 1) * 100}px`;
-  var player = document.getElementById('player');
-  player.appendChild(dice);
+  var position = `-${(roll - 1) * 100}px`;
+  dice.style.backgroundPositionX = position;
+  var board = document.getElementById(side);
+  board.appendChild(dice);
   // on aurait pu utiliser insertAdjacentHTML pour plus de choix
-  // player.insertAdjacentElement('beforeend', dice);
+  //board.insertAdjacentElement('beforeend', dice);
 }
 
 /**
@@ -26,15 +34,36 @@ function generateRandomNumber() {
  * @param {number} numberOfDices le nombre de dés qu'on veut lancer
  */
 function throwDice(numberOfDices) {
+  // plus besoin d'argument à resetBoard()
+  //resetBoard('player');
+  //resetBoard('dealer');
   resetBoard();
   for (var diceCount = 0; diceCount < numberOfDices; diceCount++) {
-    addDice();
+    addDice('player');
   }
+  /**
+   * setTimeout est une fonction qui prend:
+   * - une fontion en 1er argument
+   * - un temps en millisecondes en 2e argument
+   */
+  setTimeout(function () {
+    for (var diceCount = 0; diceCount < numberOfDices; diceCount++) {
+      addDice('dealer');
+    }
+  }, 1000);
 }
 
+/**
+ * Fonction qui reset tous les boards
+ */
 function resetBoard() {
-  var player = document.getElementById('player');
-  player.innerHTML = "";
+  //var board = document.getElementById(elementId);
+  //board.innerHTML = "";
+  // plus besoin de paramètre
+  var boards = document.querySelectorAll('.board');
+  for (var i = 0; i < boards.length; i++) {
+    boards[i].innerHTML = "";
+  }
 }
 
 throwDice(3);
